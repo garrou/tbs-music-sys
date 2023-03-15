8401e0fcf93d8edbb0fca363c6863ed3a0aab202
Adrien <39885692+1-irdA@users.noreply.github.com>
2023-03-15 14:40:45 +0100

    Add tkinter

--- script.py
+++ script.py
@@ -1,98 +1,136 @@
 import pandas as pd
+import tkinter as tk
+from tkinter import messagebox
 
-def extract_users_artists(path_data: str, min_weight: int) -> pd.DataFrame:
-    df = pd.read_csv(path_data, sep="\t")
-
-    # Keep weight greater than 100
-    return df[df['weight'] > min_weight]
-
-def create_normalized_matrix(df: pd.DataFrame) -> pd.DataFrame:
-    # Create a matrix user artist weight
-    matrix = df.pivot_table(index='userID', columns='artistID', values='weight')
-
-    # Normalize matrix
-    matrix_norm = (matrix - matrix.mean()) / matrix.std()
-
-    return matrix_norm
-
-def recommended_artists(df_similars_users_artists: pd.DataFrame, df_similars_users: pd.DataFrame):
-    artists_score = {}
-
-    # Loop through artists
-    for i in df_similars_users_artists.columns:
-        # Get the weight for artist i
-        artist_weight = df_similars_users_artists[i]
-
-        # Create a variable to store the score
-        total = 0
-
-        # Create a variable to store the number of scores
-        count = 0
-
-        # Loop through similar users
-        for u in df_similars_users.index:
-
-            # If the artist has rating
-            if pd.isna(artist_weight[u]) == False:
-                # Score is the sum of user similarity score multiply by the artist rating
-                score = df_similars_users[u] * artist_weight[u]
-
-                # Add the score to the total score for the artist so far
-                total += score
-
-                # Add 1 to the count
-                count +=1
-
-        # Get the average score for the item
-        artists_score[i] = total / count
-
-    # Convert dictionary to pandas dataframe
-    artists_score = pd.DataFrame(artists_score.items(), columns=['artistID', 'score'])
+class App(tk.Tk):
     
-    # Sort the artists by score
-    return artists_score.sort_values(by='score', ascending=False)
+    def __init__(self, height, width):
+        super().__init__()
+        self.title('Artists recommender')
+        self.resizable(width=False, height=False)
+        
+        self.canvas = tk.Canvas(self, height=height, width=width)
+        self.canvas.pack()
 
-def get_artists_to_listen_by_user(user_id: int):
+        self.frame = tk.Frame(self, bg='#080808', bd=5)
+        self.frame.place(relx=0.5, rely=0.1, relwidth=0.75, relheight=0.1, anchor='n')
+
+        self.entry = tk.Entry(self.frame, font=40)
+        self.entry.place(relwidth=0.65, relheight=1)
+
+        self.button = tk.Button(self.frame, text="Launch", font=40, command=lambda: self.check_users_and_get(self.entry.get()))
+        self.button.place(relx=0.7, relheight=1, relwidth=0.3)
+
+        self.lower_frame = tk.Frame(self, bg='#080808', bd=10)
+        self.lower_frame.place(relx=0.5, rely=0.25, relwidth=0.75, relheight=0.6, anchor='n')
+
+        self.listbox = tk.Listbox(self.lower_frame, font=40) 
+        self.listbox.place(relwidth=1, relheight=1)
+
+    def extract_users_artists(self, path_data: str, min_weight: int) -> pd.DataFrame:
+
+        df = pd.read_csv(path_data, sep="\t")
+
+        # Keep weight greater than 100
+        return df[df['weight'] > min_weight]
+
+    def create_normalized_matrix(self, df: pd.DataFrame) -> pd.DataFrame:
+
+        # Create a matrix user artist weight
+        matrix = df.pivot_table(index='userID', columns='artistID', values='weight')
+
+        # Normalize matrix
+        matrix_norm = (matrix - matrix.mean()) / matrix.std()
+
+        return matrix_norm
+
+    def recommended_artists(self, df_similars_users_artists: pd.DataFrame, df_similars_users: pd.DataFrame) -> dict[int, int]:
+
+        artists_score = {}
+
+        # Loop through artists
+        for i in df_similars_users_artists.columns:
+            # Get the weight for artist i
+            artist_weight = df_similars_users_artists[i]
+
+            # Create a variable to store the score
+            total = 0
+
+            # Create a variable to store the number of scores
+            count = 0
+
+            # Loop through similar users
+            for u in df_similars_users.index:
+
+                # If the artist has rating
+                if pd.isna(artist_weight[u]) == False:
+                    # Score is the sum of user similarity score multiply by the artist rating
+                    score = df_similars_users[u] * artist_weight[u]
+
+                    # Add the score to the total score for the artist so far
+                    total += score
+
+                    # Add 1 to the count
+                    count +=1
+
+            # Get the average score for the item
+            artists_score[i] = total / count
+
+        # Convert dictionary to pandas dataframe
+        artists_score = pd.DataFrame(artists_score.items(), columns=['artistID', 'score'])
     
-    ua_df = extract_users_artists('users_artists.dat', 100)
+        # Sort the artists by score
+        return artists_score.sort_values(by='score', ascending=False)
 
-    matrix_norm = create_normalized_matrix(ua_df)
-
-    # User similarity using Pearson correlation
-    matrix_user_similarity = matrix_norm.T.corr()
-
-    # Get similars users
-    similars_users = matrix_user_similarity[matrix_user_similarity[user_id]>0.5][user_id].sort_values(ascending=False)[:10]
-
-    # Get artists that have been listened by the user
-    user_listen = matrix_norm[matrix_norm.index == user_id].dropna(axis=1, how='all')
+    def get_artists_to_listen(self, user_id: int):
     
-    # Get artists that none of the similar users have listened
-    similars_users_artists = matrix_norm[matrix_norm.index.isin(similars_users.index)].dropna(axis=1, how='all')
+        ua_df = self.extract_users_artists('users_artists.dat', 100)
 
-    # Remove the listened artists from the artists list
-    similars_users_artists.drop(user_listen.columns,axis=1, inplace=True, errors='ignore')
+        matrix_norm = self.create_normalized_matrix(ua_df)
 
-    # Recommended artists
-    df_artists_scores = recommended_artists(similars_users_artists, similars_users)
+        # User similarity using Pearson correlation
+        matrix_user_similarity = matrix_norm.T.corr()
 
-    # Get artists and keep only name and id
-    df_artists = pd.read_csv('artists.dat', sep="\t", usecols=['id', 'name'])
+        # Get similars users
+        similars_users = matrix_user_similarity[matrix_user_similarity[user_id]>0.5][user_id].sort_values(ascending=False)[:10]
 
-    # Join artists and recommended
-    df_res = df_artists.merge(df_artists_scores, left_on='id', right_on='artistID')[['id', 'name']]
-
-    print(df_res.head(10))
-
-def main():
+        # Get artists that have been listened by the user
+        user_listen = matrix_norm[matrix_norm.index == user_id].dropna(axis=1, how='all')
     
-    df_users_artists = extract_users_artists('users_artists.dat', 100)
+        # Get artists that none of the similar users have listened
+        similars_users_artists = matrix_norm[matrix_norm.index.isin(similars_users.index)].dropna(axis=1, how='all')
 
-    user_id = int(input('Enter a user id between 2 and 2100 : '))
+        # Remove the listened artists from the artists list
+        similars_users_artists.drop(user_listen.columns,axis=1, inplace=True, errors='ignore')
 
-    if not df_users_artists[df_users_artists['userID'] == user_id].empty:
-        get_artists_to_listen_by_user(user_id)
-    else:
-        print('This user not exists')
+        # Recommended artists
+        df_artists_scores = self.recommended_artists(similars_users_artists, similars_users)
 
-main()
+        # Get artists and keep only name and id
+        df_artists = pd.read_csv('artists.dat', sep="\t", usecols=['id', 'name'])
+
+        # Join artists and recommended
+        df_res = df_artists.merge(df_artists_scores, left_on='id', right_on='artistID')[['id', 'name']]
+
+        return df_res
+
+    def generate_list(self, df: pd.DataFrame) -> None:
+
+        for artist in df.values:
+            self.listbox.insert(artist[0], artist[1])  
+
+    def check_users_and_get(self, user_id: str):
+
+        try:
+            df_users_artists = self.extract_users_artists('users_artists.dat', 100)
+            user_id = int(user_id)
+
+            if not df_users_artists[df_users_artists['userID'] == user_id].empty:
+                res = self.get_artists_to_listen(user_id)
+                self.generate_list(res)
+            else:
+                tk.messagebox.showerror(title="Error", message="This user not exists")
+        except Exception as e:
+            tk.messagebox.showerror(title="Error", message="Error during computing")
+
+App(500, 500).mainloop()
